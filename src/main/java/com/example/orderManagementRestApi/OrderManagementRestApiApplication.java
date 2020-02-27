2794326d26411b75b3308bc06cc177a915dec9a6
Maureen Josephine <Maureen Josephine>
2020-02-27 22:36:19 +0300

    inital commit

---
+++ src/main/java/com/example/orderManagementRestApi/OrderManagementRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderManagementRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/orderManagementRestApi/OrderManagementRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderManagementRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
