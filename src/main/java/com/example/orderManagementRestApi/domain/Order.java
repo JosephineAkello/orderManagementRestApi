25189abfb078cceeb1e71939a6c4cc7b885a4123
Maureen Josephine <Maureen Josephine>
2020-03-07 00:14:59 +0300

    create setters and getters

--- src/main/java/com/example/orderManagementRestApi/domain/Order.java
+++ src/main/java/com/example/orderManagementRestApi/domain/Order.java
@@ -1,4 +1,51 @@
 package com.example.orderManagementRestApi.domain;
 
-public class Order {
+import org.springframework.hateoas.service.Identifiable;
+
+public class Order implements Identifiable {
+
+    private Long id;
+    private String description;
+    private  long costInCents;
+    private boolean isComplete;
+
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getCostInCents() {
+        return costInCents;
+    }
+
+    public void setCostInCents(long cost) {
+        this.costInCents = cost;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+    
+    public void markComplete() {
+        setComplete(true);
+    }
+    public void markIncomplete() {
+        setComplete(false);
+    }
+
+    public void setComplete(boolean complete) {
+        this.isComplete = complete;
+    }
 }
