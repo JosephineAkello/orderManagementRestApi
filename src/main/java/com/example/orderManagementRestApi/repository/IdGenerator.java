7c47b52d4dd67c90e855a45e0f2aed3247c990dd
Maureen Josephine <Maureen Josephine>
2020-03-09 16:41:07 +0300

    add method for IDGeneration

--- src/main/java/com/example/orderManagementRestApi/repository/IdGenerator.java
+++ src/main/java/com/example/orderManagementRestApi/repository/IdGenerator.java
@@ -1,4 +1,18 @@
 package com.example.orderManagementRestApi.repository;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class IdGenerator {
+
+    private AtomicLong nextId = new AtomicLong(1);
+
+    public long getNextId(){
+        return nextId.getAndIncrement();
+    }
 }
