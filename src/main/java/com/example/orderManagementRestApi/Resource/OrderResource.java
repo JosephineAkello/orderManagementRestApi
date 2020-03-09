b7a1f473d67fbfffd8bad69b04b3a1bc86063695
Maureen Josephine <Maureen Josephine>
2020-03-09 16:22:55 +0300

    add getters and setters for orderresource

---
+++ src/main/java/com/example/orderManagementRestApi/Resource/OrderResource.java
@@ -0,0 +1,41 @@
+package com.example.orderManagementRestApi.Resource;
+
+import com.example.orderManagementRestApi.domain.Order;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+
+public class OrderResource extends ResourceSupport{
+
+    @JsonUnwrapped
+    private Order order;
+
+    private final long id;
+    private  final String description;
+    private final long costInCents;
+    private final boolean isComplete;
+
+    public  OrderResource(Order order){
+        id = order.getId();
+        description = order.getDescription();
+        costInCents = order.getCostInCents();
+        isComplete = order.isComplete();
+    }
+
+    @JsonProperty("id")
+    public  Long getResourceId() {
+        return id;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public  long getCostInCents(){
+        return costInCents;
+    }
+
+    public boolean isComplete(){
+        return isComplete;
+    }
+
+}
